27ec24f9e7a267ebbfc4e35155d10be154a97b11
Nathan Louie <nathanlou22@gmail.com>
2018-10-17 01:01:24 +0000

    random numbers and choice display

--- js/script.js
+++ js/script.js
@@ -10,6 +10,23 @@
 // DOCUMENT READY FUNCTION BELOW
 $(document).ready(function() {
     $("#shoot").click(function() {
+      
+var userChoice = "";
+var cpuChoice = "";
+var winner = "";
+var randomNumber = Math.random();
+
       $("#userChoice").text($("#input").val());
+      if (randomNumber > .6 && randomNumber < .9) {
+        cpuChoice = "Rock";
+      }else if(randomNumber > .3 && randomNumber < .6){
+        cpuChoice = "Paper";
+      }else{
+        cpuChoice = "Scissors";
+      }
+      
+      $("#computerChoice").text(cpuChoice);
     });
 });
+
+
